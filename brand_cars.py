2244b2b7497e0286fdc3c8fd5fb631ee336e6acf
Alexander Roman <aroman.link11@gmail.com>
2023-03-29 00:29:02 -0500

    re_start

--- brand_cars.py
+++ brand_cars.py
@@ -9,43 +9,43 @@ def main(search_terms, countries_filt, start_date, end_date, conn_string):
         
         for country in countries_filt: 
 
-            pytrends.build_payload(kw_list=[search_term], cat=67, timeframe= f'{start_date} {end_date}', geo= country, gprop='') #cat = 12 for Autos & Vehicles
+            pytrends.build_payload(kw_list=[search_term], cat=12, timeframe= f'{start_date} {end_date}', geo= country) #cat = 12 for Autos & Vehicles
             interest_over_time_df = pytrends.interest_over_time()
             interest_over_time_df = interest_over_time_df.reset_index()
             interest_over_time_df["Country"] = country
-            interest_over_time_df["Wonder of world"] = search_term
+            interest_over_time_df["Brand"] = search_term
      
             print(search_term)
             print(country)
             print(interest_over_time_df)
             df_list.append(interest_over_time_df)
             
-            print('yasta')
+            print(f'Dataframe for {search_term} and {country} added')
             time.sleep(3)
 
     final_df = pd.concat(df_list, axis=0)
 
-    print('PROCESS FINISHED')
+    print('GOOGLE TRENDS SEARCHING IS FINISHED')
     print(final_df)
     print(final_df.columns)
 
     
-"""
+
     #Transform Phase
 
     #Drop
     final_df.drop(columns=['date', 'isPartial'], index= 0 ,inplace = True)
 
     #Groupby
-    report = final_df.groupby(['Country', 'Wonder of world']).agg({'Great Wall of China' : 'sum', 'Chichen Itza' : 'sum', 'Petra' : 'sum',
-                                                'Machu Picchu' : 'sum', 'Christ the Redeemer' : 'sum', 'Colosseum' : 'sum','Taj Mahal' : 'sum'})
+    report = final_df.groupby(['Country', 'Brand']).agg({'Kia' : 'sum', 'Mitsubishi' : 'sum', 'Peugeot' : 'sum',
+                                                'Fuso' : 'sum', 'Chery' : 'sum', 'MG' : 'sum','GAC Motor' : 'sum'})
     
     #Off index
     report.reset_index(inplace = True)
 
     #Fixed table
-    selected_columns = ["Great Wall of China", "Chichen Itza", "Petra", "Machu Picchu", "Christ the Redeemer", 
-                        "Colosseum", "Taj Mahal"]
+    selected_columns = ['Kia','Mitsubishi', 'Peugeot', 'Fuso', 'Chery',
+       'MG', 'GAC Motor']
 
     row_sum = report.loc[:, selected_columns].sum(axis =1)
 
@@ -56,17 +56,18 @@ def main(search_terms, countries_filt, start_date, end_date, conn_string):
     report.drop(columns = selected_columns, axis = 0, inplace = True)
 
     #Change name of variables in the dataframe
-    report['Country'] = report['Country'].replace({'US':'United States', 'CA' : 'Canada'})
+    report['Country'] = report['Country'].replace({'AR':'Argentina', 'BO' : 'Bolivia', 'CL' : 'Chile', 'CO' : 'Colombia',
+                                                 'PE' : 'Peru'})
 
     # Create a table in the database using the dataframe
     report.to_sql(name = "2021_2022_vehicles", con = conn_string, if_exists="replace")
 
     print('ya esta')
 
-"""
+
 if __name__ == '__main__':
 
-    conn_string = "postgresql://postgres:snow@localhost:5432/seven_wonders"
+    conn_string = "postgresql://postgres:snow@localhost:5432/astara_demo"
 
     requests_args = {
     'headers': {
@@ -76,9 +77,9 @@ if __name__ == '__main__':
 
     pytrends = TrendReq(requests_args= requests_args)
 
-    search_terms = ["Great Wall of China", "Chichen Itza", "Petra", "Machu Picchu", "Christ the Redeemer", "Colosseum", "Taj Mahal"]
+    search_terms = ["Kia", "Mitsubishi", "Peugeot", "Fuso", "Chery", "MG", "GAC Motor"]
 
-    countries_filt = ["US" , "CA"]
+    countries_filt = ["AR", "BO", "CL", "CO", "PE"]
 
     start_date = '2021-01-01'
 
